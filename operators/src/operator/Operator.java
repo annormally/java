378aaa88d54205b27b6ecbac10f573f6887e2602
popacatalin <catalinpopa529@yahoo.com>
2022-04-13 10:04:19 +0300

    operator +

---
+++ operators/src/operator/Operator.java
@@ -0,0 +1,19 @@
+package operator;
+
+public class Operator {
+    public static void main(String[] args) {
+
+        // Operator +
+        int x = 50 + 50;
+        System.out.println("x = " + x);
+
+        int sum1 = 50 + 50;
+        int sum2 = sum1 + 300;
+        int sum3 = sum2 + sum2;
+
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+
+    }
+}
