0ce9f40ac75dcad81293a6191ec533200a115d8e
catalin <catalinpopa529@yahoo.com>
2022-04-14 17:36:30 +0300

    arithmetic operators

---
+++ operators/src/arithmeticOperators/ArithmeticOperators.java
@@ -0,0 +1,44 @@
+package arithmeticOperators;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+
+        // + Addition
+        int add1 = 5;
+        int add2 = 8;
+        System.out.println(add1 + add2);
+
+        // - Subtraction
+        int sub1 = 5;
+        int sub2 = 8;
+        System.out.println(sub2 - sub1);
+
+        // * Multiplication
+        int mult1 = 5;
+        int mult2 = 8;
+        System.out.println(mult1 * mult2);
+
+        // / Division
+        int div1 = 15;
+        int div2 = 3;
+        System.out.println(div1 / div2);
+
+        // % Modulo
+        int mod1 = 8;
+        int mod2 = 5;
+        System.out.println(mod1 % mod2);
+
+        // ++ Increment
+        int inc = 4;
+        System.out.println("Number: " + inc);
+        System.out.println("Increment: ");
+        System.out.println(++inc);
+
+        // -- Decrement
+        int dec = 4;
+        System.out.println("Number: " + dec);
+        System.out.println("Increment: ");
+        System.out.println(--dec);
+
+    }
+}
